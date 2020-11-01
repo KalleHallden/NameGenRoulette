210acf4d71ad24dcdfa9d523c5f66ebf13cb671c
Kalle Hallden <kallehalldeen@gmail.com>
2020-11-01 10:34:38 +0100

    first commit

---
+++ ebird_api.py
@@ -0,0 +1,37 @@
+
+from ebird.api import get_taxonomy, get_taxonomy_forms, get_taxonomy_versions
+from tokens import get_creds
+import json
+import random
+
+creds = get_creds()
+taxonomy = get_taxonomy(creds['api_key'])
+prev_name = ''
+bird_list = []
+
+for bird in taxonomy:
+	name = ''
+	try:
+		name = bird['familyComName']
+		try:
+			name = name.split(' ')[-1]
+		except:
+			pass
+		try:
+			name = name.split('-')[-1]
+		except:
+			pass
+		try:
+			name = name.split(',')[-1]
+		except:
+			pass
+		if name[-1] == 's':
+			name = name[:-1]
+		if prev_name != name:
+			prev_name = name
+			bird_list.append(name)
+	except:
+		pass
+
+n = random.randint(0,len(bird_list) -1)
+
---
+++ todo.md
@@ -0,0 +1,15 @@
+
+NameGenRoulette
+
+Generate names for startup and cross-check if the domain name exists 
+Show the user a name with available domain name 
+
+Requirements:
+
+- find a wordlist of some kind
+	- possibly animal names, common nouns
+- randomly select a word from the list
+- open up godaddy.com
+- look up the domain name 
+- if the .com is available then print name to the screen
+- let user decide whether to continue or whether to open up the site
