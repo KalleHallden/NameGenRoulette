5d146ab1bf948b2a29a365ecb835ad8a8a190556
AtherActive <34745358+AtherActive@users.noreply.github.com>
2020-11-06 21:57:24 +0100

    Simplified Print in Menu
    Just like on the domain_scraper.py, here you can put this all in a single print statement. Reduces the amount of print in your code. In this case will not do much in terms of performance, but a good thing to keep in mind.

--- menu.py
+++ menu.py
@@ -1,8 +1,10 @@
 
 def print_menu():
-	print("What do you want to do?")
-	print("----------------------------------")
-	print("--------------MENU----------------")
-	print("1. Open up godaddy.com to see more")
-	print("2. Keep searching")
-	print("3. Quit")
+	print('''
+What do you want to do?
+----------------------------------	
+--------------MENU----------------
+1. Open up godaddy.com to see more
+2. Keep searching
+3. Quit
+	''')
